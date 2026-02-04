217ccd27996eff776f17dc137fb5d156a6aa9b5b
Faisal F Rafat <faisalfaruquerafat@gmail.com>
2026-02-04 16:21:02 +0600

    Initial Commit

---
+++ bangladate/bangladateApp.swift
@@ -0,0 +1,17 @@
+//
+//  bangladateApp.swift
+//  bangladate
+//
+//  Created by Faisal F Rafat on 2/4/26.
+//
+
+import SwiftUI
+
+@main
+struct bangladateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
